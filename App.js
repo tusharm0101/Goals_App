a5551e1061fac1255ad17edc46deb85036f6c1cd
studioMac-189 <studiomac-189@studioMac-189s-iMac.local>
2022-03-03 12:27:11 -0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
+import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+import { logToConsole } from "react-native/Libraries/Utilities/RCTLog";
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+  
+  console.log(courseGoals);
+
+  const addGoalHandler = goalTitle => {
+    if (goalTitle.length === 0) {
+      return;
+    }
+   setCourseGoals(currentGoals => [
+     ...currentGoals, 
+    { id: Math.random().toString(), value: goalTitle }
+   ]);
+
+   setIsAddMode(false);
+  };
+
+  const removeGoalHandler = goalId => {
+    console.log('TO BE DELETED: ' + goalId);
+    console.log(courseGoals);    
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  }
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen} >
+      <Button  title="Add New Goal" onPress={ () => setIsAddMode(true)} />
+      <GoalInput 
+        visible={isAddMode} 
+        onAddGoal={addGoalHandler}
+        onCancel={cancelGoalAdditionHandler}
+      />
+      <FlatList 
+       keyExtractor={(item, index) => item.id}
+       data={courseGoals} 
+       renderItem={itemData => (
+        <GoalItem 
+          id={itemData.item.id}
+          onDelete={removeGoalHandler} 
+          title={itemData.item.value} 
+        />
+       )}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
 });
